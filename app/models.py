a1a443899db6a8f0d92c056db79828c071a81e0e
SmzAmir <samiezadehamir@gmail.com>
2020-12-27 20:17:09 -0500

    initial commit

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # primary key
     # relational databases can do efficient search when the attributes are indexed
     username = db.Column(db.String(64), index=True, unique=True)  # we can search user by usernames
-    email = db.Column(db.String(120), index=True, unique=True)  # we can email user by usernames
+    email = db .Column(db.String(120), index=True, unique=True)  # we can email user by usernames
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
